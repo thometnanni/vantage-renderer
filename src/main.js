e57ccbe4f1a74303b58ef2c49eef585a93ecbeed
Fidel Thomet <git@ft0.ch>
2026-03-05 15:17:15 +0100

    expose main.js in exports

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 export { VantageProjection } from './VantageProjection.js'
 export { CameraOperator } from './cameraOperator.js'
-export { VantageProjectionExporterPlugin, VantageProjectionLoaderPlugin } from './VantageGLTFPlugin.js'
+export {
+  VantageProjectionExporterPlugin,
+  VantageProjectionLoaderPlugin
+} from './VantageGLTFPlugin.js'
 export { loadTexture, setupLights } from './utils.js'
